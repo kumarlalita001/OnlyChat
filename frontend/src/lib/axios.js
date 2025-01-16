3b1fc4bfe9d3d8342f325fe91cab9fa992cd98e3
Lalit <kumarlalitamohanty@gmail.com>
2025-01-16 21:09:20 +0530

    update frontend

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,6 +4,6 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
  // baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
-  baseURL : "https://onlychat-ihv4.onrender.com/api",
+  baseURL : "https://onlychat-1.onrender.com/api",
   withCredentials: true,
 });
