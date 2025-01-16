6749caf0bac6a24c768b17225e69574715e9d679
Lalit <kumarlalitamohanty@gmail.com>
2025-01-16 20:59:44 +0530

    updated cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,7 +18,12 @@ const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
